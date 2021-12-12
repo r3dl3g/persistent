e48c4dc19025d3794d312a323e95886fd65e7941
Armin Rothfuss <armin@rothfuss-web.de>
2021-12-12 11:05:43 +0100

    Version 1.0.3. Add nix files. Change persistent_version.h location

---
+++ derivation.nix
@@ -0,0 +1,29 @@
+# https://discourse.nixos.org/t/how-to-use-a-nix-derivation-from-a-local-folder/5498/4
+
+{ stdenv, lib, pkgs } :
+
+stdenv.mkDerivation {
+  pname = "persistent";
+  version = "1.0.3";
+
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [ cmake ];
+
+  enableParallelBuilding = true;
+
+  # outputs = [ "out" ];
+
+  cmakeFlags = [
+    "-DPERSISTENT_CONFIG_INSTALL=ON"
+  ];
+
+  meta = with lib; {
+    description = "A c++ struct persistene library";
+    homepage = "https://github.com/r3dl3g/persistent";
+    changelog = "https://github.com/r3dl3g/persistent/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ r3dl3g ];
+    platforms = with platforms; linux;
+  };
+}
